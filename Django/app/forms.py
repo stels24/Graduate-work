aec5aa838cb95e16b1d5fc02da63541f3d542511
stels24 <166283589+stels24@users.noreply.github.com>
2024-11-01 10:59:42 +0300

    Update forms.py

--- Django/app/forms.py
+++ Django/app/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from app.models import Task, Category
 
-
 class TaskForm(forms.ModelForm):
+    """
+    Форма для создания и редактирования задачи.
+    """
     category = forms.ModelChoiceField(queryset=Category.objects.all())
     data_end_plan = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     data_end = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=False)
 
     class Meta:
         model = Task
-        fields = ['name', 'description', 'category', 'data_end_plan', 'status', 'data_end']
+        fields = ['name', 'description', 'category', 'data_end_plan', 'status', 
