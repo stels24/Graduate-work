80ae43764c78cad70682885adf5f80e7acd0bd19
stels24 <166283589+stels24@users.noreply.github.com>
2024-11-01 10:58:48 +0300

    Update models.py

--- Django/app/models.py
+++ Django/app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Category(models.Model):
+    """
+    Модель категории задач.
+    """
     name = models.CharField(max_length=100)
 
     class Meta:
@@ -11,8 +13,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Task(models.Model):
+    """
+    Модель задачи.
+    """
     STATUS_CHOICES = (
         ('planned', 'Запланирована'),
         ('in_progress', 'В работе'),
