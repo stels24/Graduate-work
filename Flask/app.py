1165311f08c70c9ca362a5d34ba84b7bec2ac9a5
stels24 <166283589+stels24@users.noreply.github.com>
2024-11-01 10:48:38 +0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -7,8 +7,6 @@ from wtforms import StringField, PasswordField, SubmitField, RadioField, DateFie
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_bootstrap import Bootstrap
 
-#from wtforms import StringField, SubmitField, TextAreaField, DateField, SelectField
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///siteflask.db'
@@ -17,6 +15,9 @@ Bootstrap(app)
 login_manager = LoginManager(app)
 
 class User(UserMixin, db.Model):
+    """
+    Модель пользователя для хранения информации о пользователях.
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -24,6 +25,9 @@ class User(UserMixin, db.Model):
     tasks = db.relationship('Task', backref='author', lazy=True)
 
 class Task(db.Model):
+    """
+    Модель задачи для хранения информации о задачах.
+    """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
@@ -35,44 +39,56 @@ class Task(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    """
+    Форма для регистрации пользователей.
+    """
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
 
 class LoginForm(FlaskForm):
+    """
+    Форма для входа пользователей.
+    """
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 class TaskForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    description = TextAreaField('Description')
-    category = StringField('Category', validators=[DataRequired()])
-    data_end_plan = DateField('Data End Plan', validators=[DataRequired()])
-    status = SelectField('Status', choices=[('запланирована', 'запланирована'), ('в работе', 'в работе'), ('выполнена', 'выполнена')], default='запланирована')
-    submit = SubmitField('Save Task')
-
-
+    """
+    Форма для создания и обновления задач.
+    """
+    name = StringField('Название', validators=[DataRequired()])
+    description = TextAreaField('Описание')
+    category = StringField('Категория', validators=[DataRequired()])
+    data_end_plan = DateField('Планируемая дата завершения', validators=[DataRequired()])
+    status = SelectField('Статус', choices=[('запланирована', 'запланирована'), ('в работе', 'в работе'), ('выполнена', 'выполнена')], default='запланирована')
+    submit = SubmitField('Сохранить задачу')
 
 @app.route("/")
 def home():
+    """
+    Главная страница, которая перенаправляет на страницу приветствия, если пользователь аутентифицирован, или на страницу входа в систему в противном случае.
+    """
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('login'))
 
-
-
 @login_manager.user_loader
 def load_user(user_id):
+    """
+    Функция загрузки пользователя для Flask-Login.
+    """
     return User.query.get(int(user_id))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    """
+    Страница для регистрации пользователей.
+    """
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     form = RegistrationForm()
@@ -80,12 +96,15 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Ваша учетная запись создана! Теперь вы можете войти в систему', 'success')
+        flash('Ваша учетная запись была создана! Теперь вы можете войти в систему', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Регистрация', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    """
+    Страница для входа пользователей.
+    """
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     form = LoginForm()
@@ -95,47 +114,60 @@ def login():
             login_user(user)
             return redirect(url_for('dashboard'))
         else:
-            flash('Войти не удалось. Пожалуйста, проверьте адрес электронной почты и пароль', 'danger')
-    return render_template('login.html', title='Login', form=form)
+            flash('Вход не удался. Пожалуйста, проверьте адрес электронной почты и пароль', 'danger')
+    return render_template('login.html', title='Вход', form=form)
 
 @app.route("/logout")
 def logout():
+    """
+    Страница для выхода пользователей.
+    """
     logout_user()
     return redirect(url_for('login'))
 
 @app.route("/dashboard")
 def dashboard():
+    """
+    Страница панели управления.
+    """
     if current_user.is_authenticated:
         tasks = Task.query.filter_by(author=current_user).all()
         return render_template('dashboard.html', tasks=tasks)
     else:
         return redirect(url_for('login'))
 
-
 @app.route("/task/new", methods=['GET', 'POST'])
 @login_required
 def new_task():
+    """
+    Страница для создания новой задачи.
+    """
     form = TaskForm()
     if form.validate_on_submit():
         task = Task(name=form.name.data, description=form.description.data, category=form.category.data, data_end_plan=form.data_end_plan.data, status=form.status.data, author=current_user)
         db.session.add(task)
         db.session.commit()
-        flash('Ваша задача выполнена!', 'success')
+        flash('Ваша задача была создана!', 'success')
         return redirect(url_for('dashboard'))
-    return render_template('create_task.html', title='New Task', form=form, legend='New Task')
+    return render_template('create_task.html', title='Новая задача', form=form, legend='Новая задача')
 
 @app.route("/task/<int:task_id>")
 @login_required
 def task(task_id):
+    """
+    Страница для просмотра задачи.
+    """
     task = Task.query.get_or_404(task_id)
     if task.author != current_user:
         abort(403)
     return render_template('task.html', title=task.name, task=task)
 
-
 @app.route("/task/<int:task_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_task(task_id):
+    """
+    Страница для обновления задачи.
+    """
     task = Task.query.get_or_404(task_id)
     if task.author != current_user:
         abort(403)
@@ -147,7 +179,7 @@ def update_task(task_id):
         task.data_end_plan = form.data_end_plan.data
         task.status = form.status.data
         db.session.commit()
-        flash('Ваше задание было обновлено!', 'success')
+        flash('Ваша задача была обновлена!', 'success')
         return redirect(url_for('dashboard'))
     elif request.method == 'GET':
         form.name.data = task.name
@@ -155,36 +187,34 @@ def update_task(task_id):
         form.category.data = task.category
         form.data_end_plan.data = task.data_end_plan
         form.status.data = task.status
-    return render_template('create_task.html', title='Update Task', form=form, legend='Update Task')
-
-
+    return render_template('create_task.html', title='Обновить задачу', form=form, legend='Обновить задачу')
 
 @app.route("/task/<int:task_id>/delete", methods=['POST'])
 @login_required
 def delete_task(task_id):
+    """
+    Страница для удаления задачи.
+    """
     task = Task.query.get_or_404(task_id)
     if task.author != current_user:
         abort(403)
     db.session.delete(task)
     db.session.commit()
-    flash('Ваше задание было удалено!', 'success')
+    flash('Ваша задача была удалена!', 'success')
     return redirect(url_for('dashboard'))
-
-
-# ...
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        # Создайте тестового пользователя
+        # Create a test user
         user = User.query.filter_by(username='Mike').first()
         if not user:
             user = User(username='Mike', email='mike@test.ru', password='1q2w3e')
             db.session.add(user)
             db.session.commit()
 
-        # Создайте 10 тестовых заданий
+        # Create 10 test tasks
         if Task.query.count() == 0:
             for i in range(10):
                 task = Task(name=f'Test Task {i+1}', description=f'This is test task {i+1}', category='Test', data_end_plan=datetime.utcnow() + timedelta(days=i+1), author=user)
@@ -192,3 +222,4 @@ if __name__ == '__main__':
             db.session.commit()
 
     app.run(debug=True)
+
